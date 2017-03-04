e173ec5de231eacd4ff9dea45a5aca7339a9072c
Michael Bowman <bowmanmc@gmail.com>
2017-03-04 11:50:40 -0500

    react-leaflet

--- src/app/components/HomePage.js
+++ src/app/components/HomePage.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Map, TileLayer } from 'react-leaflet';
 
 
 const HomePage = () => {
+    const position = [39.7589, -84.1916];
+
     return (
         <div className="page page__home">
-            <h1>Howdy</h1>
+            <Map center={position} zoom={13}>
+                <TileLayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png" />
+            </Map>
         </div>
     );
 };
